ee9d95f43c27f640543eb5f8c2c0d3a4ae108be4
CaptainBern <captainberneby@gmail.com>
2015-04-19 23:54:17 +0200

    yay

---
+++ Installer.php
@@ -0,0 +1,19 @@
+<?php
+namespace MessageAutoReplies;
+
+/**
+ * Class MessageAutoReplies\Installer
+ *
+ * Handles addon installation and uninstallation.
+ */
+class Installer {
+    /**
+     * Install the addon.
+     */
+    public static function install() {}
+
+    /**
+     * Uninstalls the addon.
+     */
+    public static function uninstall() {}
+}
