dbe0ad6e879d79428bbedffc5849c5e3e57fae20
CaptainBern <captainberneby@gmail.com>
2015-04-22 17:51:20 +0200

    Work on the installer

--- Installer.php
+++ Installer.php
@@ -1,5 +1,6 @@
 <?php
 namespace MessageAutoReplies;
+use XenForo_Application;
 
 /**
  * Class MessageAutoReplies\Installer
@@ -7,13 +8,22 @@ namespace MessageAutoReplies;
  * Handles addon installation and uninstallation.
  */
 class Installer {
+
     /**
      * Install the addon.
      */
-    public static function install() {}
+    public static function install() {
+        /** @var \Zend_Db_Adapter_Abstract $db */
+        $db = XenForo_Application::get('db');
+        $db->query("CREATE TABLE IF NOT EXISTS xf_mar_messages (user_id INT UNSIGNED NOT NULL UNIQUE PRIMARY KEY, message_contents TEXT NOT NULL)");
+    }
 
     /**
      * Uninstalls the addon.
      */
-    public static function uninstall() {}
+    public static function uninstall() {
+        /** @var \Zend_Db_Adapter_Abstract $db */
+        $db = XenForo_Application::get('db');
+        $db->query("DROP TABLE IF EXISTS xf_mar_messages");
+    }
 }
