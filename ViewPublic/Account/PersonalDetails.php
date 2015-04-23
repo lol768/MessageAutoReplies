b901b46317db9984a8390a2cfdd7b3b28bd5b466
lol768 <lol768@users.noreply.github.com>
2015-04-23 19:16:34 +0100

    Nicely done, @lol768

--- ViewPublic/Account/PersonalDetails.php
+++ ViewPublic/Account/PersonalDetails.php
@@ -3,7 +3,7 @@
 namespace MessageAutoReplies\ViewPublic\Account;
 
 class PersonalDetails extends XFCP_PersonalDetails {
-    public function renderHtml({
+    public function renderHtml() {
         $this->_params['autoResponderEditor'] = XenForo_ViewPublic_Helper_Editor::getEditorTemplate(
             $this, 'auto_responder', $this->_params['autoResponder']
         );
