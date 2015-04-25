08d33300a48ad39bc72f3596b68b6273dcb72370
lol768 <lol768@lol768.com>
2015-04-25 15:41:23 +0100

    Add test class for CaptainBern :D

---
+++ Test/FormatterCollectionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace MessageAutoReplies\Test;
+
+
+use PHPUnit_Framework_TestCase;
+
+class FormatterCollectionTest extends PHPUnit_Framework_TestCase {
+
+    public function testSimpleFormatter() {
+        // TODO: CaptainBern!
+    }
+
+}
