6c81f5b8a8e1f229393749284504d49899c12e7c
lol768 <lol768@users.noreply.github.com>
2015-04-25 13:42:59 +0100

    Add another function to make the DW shut up

--- Model/AutoResponse.php
+++ Model/AutoResponse.php
@@ -9,4 +9,8 @@ class AutoResponse extends XenForo_Model {
     public function getEntryByUserId($userId) {
         return $this->_getDb()->fetchOne('SELECT message_contents FROM xf_mar_messages WHERE user_id = ?', $userId);
     }
+    
+    public function getEntireEntryByUserId($userId) {
+        return $this->_getDb()->fetchRow('SELECT message_contents FROM xf_mar_messages WHERE user_id = ?', $userId);
+    }
 }
