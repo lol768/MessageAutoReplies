5e8c5cf40ceb4f94222aa6d1d75cbdfbb5f6e990
CaptainBern <captainberneby@gmail.com>
2015-04-25 17:07:24 +0200

    lol768'd the whole thing :D

--- Formatters/FormatterCollection.php
+++ Formatters/FormatterCollection.php
@@ -4,13 +4,13 @@ namespace MessageAutoReplies\Formatters;
 
 class FormatterCollection {
 
-    public static $formatPattern = "((\\{)(\\w*)(}))";
+    const FORMAT_PATTERN = "((\\{)(\\w*)(}))";
 
-    public static $formatters;
+    public $formatters;
 
     public static function format($message) {
         $offset = 0;
-        while(preg_match($message, self::$formatPattern, $matches, PREG_OFFSET_CAPTURE, $offset)){
+        while(preg_match($message, self::FORMAT_PATTERN, $matches, PREG_OFFSET_CAPTURE, $offset)){
             $offset = $matches[0][1] + strlen($matches[0][0]);
         }
     }
