1c0e25f7134b69709ae30c354670e705cab21234
CaptainBern <captainberneby@gmail.com>
2015-04-26 21:29:26 +0200

    Added data formatter-test

--- Test/FormatterCollectionTest.php
+++ Test/FormatterCollectionTest.php
@@ -10,14 +10,30 @@ class FormatterCollectionTest extends PHPUnit_Framework_TestCase {
 
     public function testSimpleFormatter() {
         $fc = new FormatterCollection();
+
         // Make a simple formatter which changes {project} to "MCExchange"
         $projectFormatter = new AnonymousFormatter();
         $projectFormatter->format = function($data) {
-            // !! Why do I need $message here? I'm replacing {project} only?
             return "MCExchange";
         };
+
         $fc->registerFormatter("project", $projectFormatter);
+
         $data = array();
         $this->assertSame("Hello from MCExchange", $fc->format("Hello from {project}", $data));
+    }
+
+    public function testFormatterWithdata() {
+        $fc = new FormatterCollection();
+
+        $dataFormatter = new AnonymousFormatter();
+        $dataFormatter->format = function($data) {
+            return $data['custom_data'];
+        };
+
+        $fc->registerFormatter("custom_data", $dataFormatter);
+
+        $data = ['custom_data' => "Some custom data!"];
+        $this->assertSame("Custom data: Some custom data!", $fc->format("Custom data: {custom_data}", $data));
     }
 }
