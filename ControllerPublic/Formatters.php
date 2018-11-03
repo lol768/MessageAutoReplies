48f420dd784ebdadf0db769b469de9860d5c5246
Jayson Fong <contact@jaysonfong.me>
2018-11-03 16:15:00 -0400

    Fixed a Potential Class Not Found Issue

--- ControllerPublic/Formatters.php
+++ ControllerPublic/Formatters.php
@@ -7,6 +7,6 @@ use XenForo_ControllerPublic_Abstract;
 class Formatters extends XenForo_ControllerPublic_Abstract {
 
     public function actionShowHelp() {
-        return $this->responseView('MessageAutoReplies\ViewPublic\Formatters', 'mar_formatters');
+        return $this->responseView(null, 'mar_formatters');
     }
 } 
